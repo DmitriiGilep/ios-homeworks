1abf18dc020afcbf5ff548cee1e691457de5aebf
Dmitrii Gilep <dmitry.gilep@gmail.com>
2022-02-12 23:13:21 +0300

    new commit

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by DmitriiG on 10.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let profileView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            view.addSubview(profileView)
+            profileView.nameOfUser.text = "Dmitry"
+            
+        }
+    }
+}
---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by DmitriiG on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
